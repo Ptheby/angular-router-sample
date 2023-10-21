fb3a322229bc00a1c3c0296f6e69ca9d6ad9a5c8
Paul Theby <paultheby@gmail.com>
2023-10-20 22:24:56 -0500

    New Simpler Routing Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+{path: 'crisis-list', component: CrisisListComponent},
+{path: 'heroes-list', component: HeroesListComponent},
+{path:'', redirectTo:'/heroes-list', pathMatch:'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
